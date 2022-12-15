7becec4c124d413a0c987e9f4fc17910c71c6397
MuhinduRich <jr.muhindu.waweru@gmail.com>
2022-12-14 23:44:51 -0800

    putchar

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_alaphabet - prints the alpha in the lowercase
+ */
+void print_alphabet(void)
+{
+          char la:
+
+	 for (la = 'a'; la <= 'z'; la++)	
+	       _putchar(la);
+
+          _putchar('\n');
+
+}	  
